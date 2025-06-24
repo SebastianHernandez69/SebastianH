3989a0f49dd0055af34f13d7d75cfd68e3e41603
SebastianHernandez69 <sh3641046@gmail.com>
2025-06-23 21:19:55 -0600

    fix: updating astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        plugins: [tailwindcss()]
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                '@': path.resolve('./src'), // ⬅️ aquí defines el alias
+                '@components': path.resolve('./src/components'),
+                '@layouts': path.resolve('./src/layouts'),
+                '@pages': path.resolve('./src/pages'),
+                '@styles': path.resolve('./src/styles'),
+                '@utils': path.resolve('./src/utils'),
+            },
+        },
     }
 });
