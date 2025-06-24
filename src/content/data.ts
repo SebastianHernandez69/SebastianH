35b6c0ab930ac72f878b4fdcaf7ea0cd8db0127d
SebastianHernandez69 <sh3641046@gmail.com>
2025-06-23 21:13:33 -0600

    feat: adding skills section

---
+++ src/content/data.ts
@@ -0,0 +1,77 @@
+// src/content/data.js
+
+import JavaScript from "@/icons/tech/front/JavaScript.astro";
+import TypeScript from "@/icons/tech/front/TypeScript.astro";
+import Nextjs from "@/icons/tech/front/Nextjs.astro";
+import React from "@/icons/tech/front/React.astro";
+import ReactQuery from "@/icons/tech/front/ReactQuery.astro";
+import Tailwind from "@/icons/tech/front/Tailwind.astro";
+
+import Nodejs from "@/icons/tech/back/Nodejs.astro";
+import Nestjs from "@/icons/tech/back/Nestjs.astro";
+import Dotnet from "@/icons/tech/back/Dotnet.astro";
+import SpringBoot from "@/icons/tech/back/SpringBoot.astro";
+import Java from "@/icons/tech/back/Java.astro";
+import Csharp from "@/icons/tech/back/Csharp.astro";
+import Graphql from "@/icons/tech/back/Graphql.astro";
+
+import MySql from "@/icons/tech/db/MySql.astro";
+import Postgres from "@/icons/tech/db/Postgre.astro";
+import Mongodb from "@/icons/tech/db/Mongodb.astro";
+import Sqlserver from "@/icons/tech/db/Sqlserver.astro";
+import Prisma from "@/icons/tech/db/Prisma.astro";
+
+// tools
+import Git from "@/icons/tech/tools/Git.astro";
+import Aws from "@/icons/tech/tools/Aws.astro";
+import Azure from "@/icons/tech/tools/Azure.astro";
+import Docker from "@/icons/tech/tools/Docker.astro";
+import Terraform from "@/icons/tech/tools/Terraform.astro";
+import Railway from "@/icons/tech/tools/Railway.astro";
+
+export const TECH_STACK = [
+  {
+    section: "FRONTEND",
+    data: [
+      { title: "JavaScript", Icon: JavaScript },
+      { title: "TypeScript", Icon: TypeScript },
+      { title: "Next.js", Icon: Nextjs },
+      { title: "React", Icon: React },
+      { title: "React Query", Icon: ReactQuery },
+      { title: "Tailwind CSS", Icon: Tailwind },
+    ],
+  },
+  {
+    section: "BACKEND",
+    data: [
+      { title: "Node.js", Icon: Nodejs },
+      { title: "NestJS", Icon: Nestjs },
+      { title: ".NET", Icon: Dotnet },
+      { title: "Spring Boot", Icon: SpringBoot },
+      { title: "Java", Icon: Java },
+      { title: "C#", Icon: Csharp },
+      { title: "GraphQL", Icon: Graphql },
+    ],
+  },
+  {
+    section: "DATABASES",
+    data: [
+      { title: "MySQL", Icon: MySql },
+      { title: "PostgreSQL", Icon: Postgres },
+      { title: "MongoDB", Icon: Mongodb },
+      { title: "SQL Server", Icon: Sqlserver },
+      { title: "Prisma", Icon: Prisma },
+    ],
+  },
+  {
+    section: "TOOLS",
+    data: [
+      { title: "Git", Icon: Git },
+      { title: "AWS", Icon: Aws },
+      { title: "Azure", Icon: Azure },
+      { title: "Docker", Icon: Docker },
+      { title: "Terraform", Icon: Terraform },
+      { title: "Railway", Icon: Railway },
+    ],
+  },
+];
--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -1,4 +1,8 @@
+import Lenis from '@studio-freight/lenis/types';
 import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener('DOMContentLoaded', () => {
   gsap.from(".hero-text", {
@@ -17,3 +21,98 @@ document.addEventListener('DOMContentLoaded', () => {
     ease: "power3.out",
   })
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const techTimeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".scroll-content",
+      start: "top bottom",
+      end: "bottom top", 
+      scrub: 1,         
+    }
+  });
+
+  techTimeline
+  .to(".fond-circle", {
+    y: -40,
+    opacity: 0,
+    duration: 0.8,
+    stagger: 0.3,
+    ease: "power3.out",
+  })
+  .to(".hero-text", {
+    y: -40,
+    opacity: 0,
+    duration: 0.7,
+    stagger: 0.3,
+    ease: "power3.out",
+  }, "<")
+  .to(".contact-button", {
+    y: -40,
+    opacity: 0,
+    duration: 0.8,
+    stagger: 0.3,
+    ease: "power3.out",
+  })
+  .to(".hero-arrow", {
+    y: -40,
+    opacity: 0,
+    duration: 0.8,
+    stagger: 0.3,
+    ease: "power3.out",
+  }, "<")
+  .from(".tech-item", {
+    y: 40,
+    opacity: 0,
+    stagger: 0.15,
+    duration: 0.6,
+    ease: "power3.out",
+  }, "<");
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// gsap.utils.toArray('.sticky-title').forEach((title, index) => {
+//   let container = title.parentElement;
+//   let content = title.parentElement.nextElementSibling;
+
+//   ScrollTrigger.create({
+//     trigger: title.parentElement,
+//     start: "top top",
+//     endTrigger: content,
+//     end: "bottom top",
+//     pin: container,
+//     pinSpacing: false,
+//     scrub: true,
+//   });
+// });
+
+// gsap.from(".tech-item", {
+//   y: 40,
+//   opacity: 0,
+//   duration: 0.6,
+//   stagger: 0.15, // Más suave y fluido
+//   ease: "power3.out",
+//   scrollTrigger: {
+//     trigger: ".scroll-content", // Usa el contenedor común
+//     start: "top 80%",
+//     toggleActions: "play none none reverse"
+//   }
+// });
